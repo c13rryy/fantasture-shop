b3b91137d53fcb2509eaece95cea6315d6d5a2ac
c13rryy <spell20044@gmail.com>
2024-02-15 15:10:46 +0300

    feat: remove-link

--- src/components/ProductComponents/ProductRange/ProductRange.tsx
+++ src/components/ProductComponents/ProductRange/ProductRange.tsx
@@ -1,7 +1,5 @@
 import Typo from "../../ui/typography/typo";
 import Image, { StaticImageData } from "next/image";
-import Link from "next/link";
-
 interface ProductItemProps {
   src: StaticImageData;
   title: string;
@@ -9,10 +7,7 @@ interface ProductItemProps {
 
 const ProductRange = ({ src, title }: ProductItemProps) => {
   return (
-    <Link
-      href="/"
-      className="flex sm:flex-col flex-col-reverse sm:gap-40px gap-16px"
-    >
+    <div className="flex sm:flex-col flex-col-reverse sm:gap-40px gap-16px">
       <div className="relative sm:w-[384px] w-full 2xl:h-[480px] xl:h-[400px] sm:h-[350px] h-[300px] transition-opacity hover:opacity-50">
         <Image
           src={src}
@@ -26,7 +21,7 @@ const ProductRange = ({ src, title }: ProductItemProps) => {
       </div>
 
       <Typo className="sm:text-center" tag="h3" text={title} />
-    </Link>
+    </div>
   );
 };
 
