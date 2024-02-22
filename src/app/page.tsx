0cdee247c5cf195b0ed3306f39d83734baf58f7a
c13rryy <spell20044@gmail.com>
2024-02-22 12:11:16 +0300

    feat: add loader to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-/* import { db } from "@/lib/db"; */
+import { db } from "@/lib/db";
 import Logo from "@/screens/MainPage/Logo/Logo";
-/* import Products from "@/screens/MainPage/Products/Products"; */
+import Products from "@/screens/MainPage/Products/Products";
 import ProductsRange from "@/screens/MainPage/ProductsRange/ProductsRange";
 import RoomsInspiration from "@/screens/MainPage/RoomsInspiration/RoomsInspiration";
-/* import { INFINITE_SCROLL_PAGINATION_RESULTS } from "@/constants"; */
+import { INFINITE_SCROLL_PAGINATION_RESULTS } from "@/constants";
+import { Suspense } from "react";
+import { Icon } from "@/components/ui/Icon/Icon";
 
-export default async function Home() {
-  /*  const products = await db.product.findMany({
+async function ProductLoad() {
+  const products = await db.product.findMany({
     orderBy: {
       createdAt: "desc",
     },
@@ -15,13 +17,27 @@ export default async function Home() {
       votes: true,
     },
     take: INFINITE_SCROLL_PAGINATION_RESULTS,
-  }); */
+  });
 
+  return <Products data={products} />;
+}
+
+export default async function Home() {
   return (
     <>
       <Logo />
       <ProductsRange />
-      {/* <Products data={products} /> */}
+      <Suspense
+        fallback={
+          <div className="h-[100vh] flex justify-center items-center">
+            <div className="animate-spin">
+              <Icon icon="loader" size={32} />
+            </div>
+          </div>
+        }
+      >
+        <ProductLoad />
+      </Suspense>
       <RoomsInspiration />
     </>
   );
