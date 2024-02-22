51e42e6947a0f1fd5a96120f55a393b51c5d7f62
c13rryy <spell20044@gmail.com>
2024-02-22 11:46:13 +0300

    feat: change og image

---
+++ src/app/opengraph-image.tsx
@@ -0,0 +1,44 @@
+import { ImageResponse } from "next/og";
+
+export const alt = "About Shop";
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = "image/png";
+
+export default async function Image() {
+  const interSemiBold = fetch(
+    new URL("./Inter-SemiBold.ttf", import.meta.url)
+  ).then(res => res.arrayBuffer());
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 128,
+          background: "white",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        Fantasture Shop
+      </div>
+    ),
+    {
+      ...size,
+      fonts: [
+        {
+          name: "Inter",
+          data: await interSemiBold,
+          style: "normal",
+          weight: 400,
+        },
+      ],
+    }
+  );
+}
